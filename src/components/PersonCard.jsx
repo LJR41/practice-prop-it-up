83577f47f6418ccd0c838018db6ebb3ca0df347b
LJR411~ <ljremulla@yahoo.com>
2023-08-02 14:37:52 -0700

    putting it together commit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 const PersonCard = (props) => {
     const {firstName, lastName, age, hairColor} = props
+    //   variable      function to update the variable  = default value
+    const[ageActual, setAgeActual] =useState(age)
+
+    const haveBirthday = ()=>{
+        setAgeActual(ageActual + 1)
+
+    }
     return(
         <div>
             <h1 style={{color:"red"}}>Instructor {firstName} {lastName}</h1>
-            <p>Age:{age}</p>
+            <p>Age:{ageActual}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={haveBirthday}>Happy Birthday</button>
         </div>
     );
 }
-export default PersonCard;
+export default PersonCard; 
 
 // Pair Programming with Cohort.
